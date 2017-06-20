fffafe4ac568da54b77fa699972963114e6613c3
Schmidt <csat6332@uibk.ac.at>
2017-06-20 21:27:43 +0200

    Signed-off-by: Schmidt <csat6332@uibk.ac.at>

--- projekt/main/js/skript.js
+++ projekt/main/js/skript.js
@@ -97,7 +97,7 @@ window.onload = function() {
 				current = current % backgrounds.length;
 				back.css('background-image', backgrounds[current]);
 			}
-			setInterval(nextBackground, 20000);
+			setInterval(nextBackground, 50000);
 
 			back.css('background-image', backgrounds[0]);
 				 
